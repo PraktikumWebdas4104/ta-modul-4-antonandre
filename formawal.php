20e3c11be93cde32ffe55000d2ed8b44b568c8f2
antonandre <43515471+antonandre@users.noreply.github.com>
2018-09-29 23:32:05 +0700

    Update formawal.php
    ANTONIUS ANDRE YUWONO PUTRANTO
    6701170015
    D3MI-41-04

--- formawal.php
+++ formawal.php
@@ -1,5 +1,9 @@
+<form action="proses.php" method="POST">
+	<h1><marquee>MENU LOGIN</marquee></h1>
+			USERNAME :<input type="text" name="uname"><br>
+			PASSWORD :<input type="password" name="pass"><br>
+			<input type="submit" name="submit" value="Kirim">
+</form>
 <?php
-
-
 
 ?>
